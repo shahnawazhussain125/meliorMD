c5573287d0be394f84ad344dee9e019ed0b9e182
Shahnawaz Hussain <shahnawazhussain125@gmail.com>
2019-09-05 02:31:04 +0500

    CSS Commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Header from './Components/Header';
 import ProviderList from './Pages/ProviderList';
 // import Footer from './Components/Footer';
 import SearchResult from './Components/SearchResult';
+import Search from './Components/Search';
 
 function App() {
   return (
     <div>
       <Header/>
-      <ProviderList/>
+      {/* <ProviderList/> */}
       {/* <Footer/> */}
       {
-        // <SearchResult/>
+        <SearchResult/>
       }
+      {/* <Search/> */}
     </div>
   );
 }
---
+++ src/Components/Search/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Row, Col, Input, Button, Icon, Tabs, Radio, Breadcrumb, Select, Pagination } from 'antd';
+import 'antd/dist/antd.css';
+import './index.css';
+
+const InputGroup = Input.Group;
+const { TabPane } = Tabs;
+const { Option } = Select;
+
+class Search extends Component {
+
+    render() {
+      return (
+        <div className="provoder-list-body">
+            <Row className="row-container">
+            <Col style={{height:'100%'}}>
+                <div className="div-container" style={{backgroundColor:'#75A6CF'}}>
+                    Hello world ksjdfjzdj
+                </div>
+            </Col>
+            </Row>
+        </div>
+      )
+    }
+}
+
+
+export default Search;
