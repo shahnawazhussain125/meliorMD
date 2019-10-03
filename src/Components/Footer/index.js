cca09b4a29ea75125f38d8a38bfb34ffe951f645
ZainHasan97-Razi <zainrazi97@yahoo.com>
2019-10-03 14:51:39 +0500

    Update

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -14,63 +14,77 @@ class Footer extends Component {
         return (
             <div className="footer-div">
                 <Row className="footer">
-                    <Col span={2} ></Col>
-                    <Col span={6} className="footer-logo-container">
+                    <Col lg={2} md={1} sm={0} xs={0}></Col>
+
+                    <Col lg={6} md={6} sm={6} xs={7} className="footer-logo-container">
                         <Row className="footer-logo">
-                            <img src={logo} alt="logo" />
+                            <img className="melior-logo" src={logo} alt="logo" />
                         </Row>
                         <Row className="footer-logo-text">
                             <p>
-                                MeliorMD is a one-stop shop for finding healthcare providers
-                                <br />
+                                MeliorMD is a one stop shop for finding health care providers 
                                 across specialties. Finding a healthcare provider can be
                             </p>
                         </Row>
                     </Col>
 
-                    <Col span={4} className="border-black">
+                    <Col lg={0} md={0} sm={0} xs={1}></Col>
+
+                    <Col lg={4} md={4} sm={4} xs={6} className="border-black">
                         <Row className="links-title">
                             <h4>FEATURES</h4>
                         </Row>
                         <Row className="links-list">
                             <ul>
-                                <li>Find Doctors</li>
-                                <li>Add a provider listing</li>
-                                <li>About</li>
-                                <li>Mission</li>
+                                <li className="li-padding">Find Doctors</li>
+                                <li className="li-padding">Add a provider listing</li>
+                                <li className="li-padding">About</li>
+                                <li className="li-padding">Mission</li>
                             </ul>
                         </Row>
                     </Col>
 
-                    <Col span={4} className="border-black">
+                    <Col lg={4} md={4} sm={4} xs={5} className="border-black">
                         <Row className="links-title">
-                            <h4>Company</h4>
+                            <h4>COMPANY</h4>
                         </Row>
                         <Row className="links-list">
                             <ul>
-                                <li>About Us</li>
-                                <li>Pricing</li>
-                                <li>Careers</li>
-                                <li>Contact Us</li>
+                                <li className="li-padding">About Us</li>
+                                <li className="li-padding">Pricing</li>
+                                <li className="li-padding">Careers</li>
+                                <li className="li-padding">Contact Us</li>
                             </ul>
                         </Row>
                     </Col>
 
-                    <Col span={4} className="border-black">
+                    <Col lg={0} md={0} sm={0} xs={1}></Col>
+
+                    <Col lg={4} md={4} sm={4} xs={4} className="border-black">
                         <Row className="links-title">
                             <h4>FOLLOW</h4>
                         </Row>
-                        <Row>
-                            <div className="icons-list">
-                                <Icon type="linkedin" className="icon" />
-                                <IconFont type="icon-facebook" className="icon" />
-                                <IconFont type="icon-twitter" className="icon" />
-                            </div>
+                        <Row type="flex">
+                                <Col lg={6} md={6} sm={6} xs={24}>
+                                    <Icon type="linkedin" className="icon" />
+                                </Col>
+
+                                <Col lg={1} md={1} sm={1} xs={0}></Col>
+
+                                <Col lg={6} md={6} sm={6} xs={24}>
+                                    <IconFont type="icon-facebook" className="icon" />
+                                </Col>
+
+                                <Col lg={1} md={1} sm={1} xs={0}></Col>
+
+                                <Col lg={6} md={6} sm={6} xs={24}>
+                                    <IconFont type="icon-twitter" className="icon" />
+                                </Col>
                             <br />
                         </Row>
                     </Col>
 
-                    <Col span={2} ></Col>
+                    <Col lg={2} md={1} sm={0} xs={0}></Col>
                 </Row>
 
                 <Row type="flex" justify="space-around">
