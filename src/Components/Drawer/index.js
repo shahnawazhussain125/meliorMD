424e96868cd0da2b1d4500eedaea64647b853e1a
ZainHasan97-Razi <zainrazi97@yahoo.com>
2019-10-06 14:48:22 +0500

    Update+

---
+++ src/Components/Drawer/index.js
@@ -0,0 +1,37 @@
+import { Drawer } from 'antd';
+
+class App extends React.Component {
+  state = { visible: false };
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Drawer
+          title="Basic Drawer"
+          placement="right"
+          closable={false}
+          onClose={this.onClose}
+          visible={this.state.visible}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, mountNode);
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Button, Icon } from 'antd';
 import 'antd/dist/antd.css';
 import './index.css';
 import logo from '../../assets/images/md-logo.webp';
+import DrawerTab from '../Drawer/index'
 
 class Header extends Component {
+  state = { visible: false };
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
 
     render() {
         return (
-            <div>
+            <div className="header-div">
                 <Row type="flex" className="header-bar">
-                    <Col span={4}>
+                <Col lg={5} md={5} sm={8} xs={8}>
+                    <Col span={6}>
+                        <Button className="side-bar" onClick={this.showDrawer}>
+                            <Icon type="menu" />
+                        </Button>
+                    </Col>
+                    <Col span={14}>
                         <img className="header-logo" src={logo} alt="logo" />
                     </Col>
-                    <Col span={16}>
+                </Col>
+                    <Col lg={15} md={15} sm={0} xs={0}>
                         <div className="navbar">
                             <ul className="navbar-links">
                                 <li>ABOUT</li>
@@ -23,7 +44,7 @@ class Header extends Component {
                             </ul>
                         </div>
                     </Col>
-                    <Col span={4}>
+                    <Col lg={4} md={4} sm={8} xs={8}>
                         <div className="vl">
                             <div>
                                 <Button type="primary" className="provider-button">Provider Login</Button>
