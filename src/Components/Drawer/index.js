85aad82d2cd1dea9ad8a451d64d5335a87095a5b
ZainHasan97-Razi <zainrazi97@yahoo.com>
2019-10-07 13:18:40 +0500

    Update

--- src/Components/Drawer/index.js
+++ src/Components/Drawer/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 
-import { Drawer } from 'antd';
+import { Drawer, Row, Col } from 'antd';
 
 class CustomDrawer extends Component {
-  state = { visible: false };
+  state = { visible: true };
 
   showDrawer = () => {
     this.setState({
@@ -19,17 +19,23 @@ class CustomDrawer extends Component {
 
   render() {
     return (
-      <div>
+      <div className="side-bar-div">
         <Drawer
-          title="Basic Drawer"
+        style={{textDecoration:'#2c61ad'}}
+          title="Tab to view page"
           placement="right"
           closable={false}
           onClose={this.onClose}
           visible={this.state.visible}
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          <Row type="flex" justufy="center">
+            <Col span={24}>
+              <Row type="flex" className="sidebar-tabs"><h4>ABOUT</h4></Row>
+              <Row type="flex" className="sidebar-tabs"><h4>DIRECTORY</h4></Row>
+              <Row type="flex" className="sidebar-tabs"><h4>CONTACT US</h4></Row>
+              <Row type="flex" className="sidebar-tabs"><h4>ADD A PROVIDER LIST</h4></Row>
+            </Col>
+          </Row>
         </Drawer>
       </div>
     );
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -19,7 +19,7 @@ class Header extends Component {
                         <img className="header-logo" src={logo} alt="logo" />
                     </Col>
                 </Col>
-                    <Col lg={15} md={15} sm={0} xs={0}>
+                    <Col lg={16} md={15} sm={0} xs={0}>
                         <div className="navbar">
                             <ul className="navbar-links">
                                 <li>ABOUT</li>
@@ -29,7 +29,8 @@ class Header extends Component {
                             </ul>
                         </div>
                     </Col>
-                    <Col lg={4} md={4} sm={8} xs={8}>
+                    <Col lg={0} md={0} sm={10} xs={8}></Col>
+                    <Col lg={3} md={4} sm={6} xs={8}>
                         <div className="vl">
                             <div>
                                 <Button type="primary" className="provider-button">Provider Login</Button>
