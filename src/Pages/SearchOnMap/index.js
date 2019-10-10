0bec5b3a02b35aff27f8ec673703775cfabd6d5e
shahnawazhussain125 <shahnawazhussain125@gmail.com>
2019-10-11 00:44:35 +0500

    Search On Map Update

--- src/Pages/SearchOnMap/index.js
+++ src/Pages/SearchOnMap/index.js
@@ -22,9 +22,15 @@ class SearchOnMap extends Component {
         this.setState({ ...this.props.searchResult })
     }
 
+    camelCaseAddress = (address) =>{
+        return address.toLowerCase()
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+    }
+
     render() {
         const { results, result_count } = this.state;
-        console.log('Resuilt ==>', this.state)
         return (
             <div>
                 <Row>
@@ -47,9 +53,9 @@ class SearchOnMap extends Component {
                                 </Breadcrumb>
                             </Col>
                         </Row>
-                        <Row className="search-row">
+                        <Row type="flex" align="middle" className="search-row">
                             <Col lg={10} md={10} sm={24} xs={24}>
-                                <span className="num-doctors">{result_count} <span style={{color:'#727573'}}> Doctors</span></span>
+                                <span className="num-doctors">{result_count} <span style={{ color: '#727573' }}> Doctors</span></span>
                             </Col>
                             <Col lg={1} md={1} sm={1} xs={1}></Col>
                             <Col lg={3} md={3} sm={5} xs={4}><div className="more-filter">More Filters</div></Col>
@@ -85,54 +91,54 @@ class SearchOnMap extends Component {
                                 </Select>
                             </Col>
                         </Row>
-                        {
-            //================== Card from here =========================//
-                            results.map((value, index) => {
-                                return (
-                                    <Col lg={24} md={24} sm={24} xs={24}>
-                                    <Row type="flex" justify="center" className="card-style" key={index}>
-                                        <Col xl={5} lg={6} md={6} sm={6} xs={6}>
-                                            <Row>
-                                                <img src={dr_image} alt="dr_image" width="100%"/>
-                                            </Row>
-                                        </Col>
-                                        <Col xl={19} lg={18} md={18} sm={18} xs={18}>
-                                            <Row type="flex" className="class-detail-row">
-                                                <div >
-                                                    <h4>{value.basic.name}, {value.basic.credential}</h4>
-                                                    <Col xxl={17} xl={16} lg={15} md={15} sm={15} xs={15}>
-                                                        <Rate className="rate-icons" disabled defaultValue={4} />
-                                                        <p className="patient-rating">108 Patient Satisfaction Ratings<br />10 Patient Comments</p>
-                                                    </Col>
-                                                    <Col xxl={7} xl={8} lg={9} md={9} sm={9} xs={9}>
-                                                        {/* <Row type="flex"></Row>
-                                                        <Row type="flex"></Row> */}
-                                                        <Row type="flex">
-                                                            <h5 className="provider-info-title">DEPARTMENTS</h5>
-                                                            <Typography className="provider-info">{value.taxonomies[0].desc}</Typography>
-                                                            <h5 className="provider-info-title">LOCATIONS</h5>
-                                                            <Typography className="provider-info">{value.addresses[0].address_1} </Typography>
-                                                        </Row>
-                                                    </Col>
-                                                </div>
-                                            </Row>
-                                            <Row type="flex">
-                                                <Col className="view-on-map-col" xxl={18} xl={17} lg={15} md={13} sm={14} xs={15}><h6>View on Map | Directions</h6></Col>
-                                                <Col xxl={6} xl={7} lg={9} md={11} sm={10} xs={9}>
-                                                    <Button className="button-appointment">
-                                                        <Row type="flex" justify="space-around">
-                                                                <Col lg={10} md={10} sm={24} xs={24}>Request </Col>
-                                                                <Col lg={14} md={14} sm={24} xs={24}>appointment</Col>  
-                                                        </Row>
-                                                    </Button>
+                        <Row className="provide-list-container">
+                            {
+                                //================== Card from here =========================//
+                                results.map((value, index) => {
+                                    return (
+                                        <Col lg={24} md={24} sm={24} xs={24} key={index}>
+                                            <Row type="flex" justify="center" className="card-style">
+                                                <Col xl={5} lg={6} md={6} sm={6} xs={6}>
+                                                    <Row>
+                                                        <img src={dr_image} alt="dr_image" width="100%" />
+                                                    </Row>
+                                                </Col>
+                                                <Col xl={19} lg={18} md={18} sm={18} xs={18}>
+                                                    <Row type="flex" className="class-detail-row">
+                                                        <div >
+                                                            <h4>{value.basic.name}, {value.basic.credential}</h4>
+                                                            <Col xxl={17} xl={16} lg={15} md={15} sm={15} xs={15}>
+                                                                <Rate className="rate-icons" disabled defaultValue={4} />
+                                                                <p className="patient-rating">108 Patient Satisfaction Ratings<br />10 Patient Comments</p>
+                                                            </Col>
+                                                            <Col xxl={7} xl={8} lg={9} md={9} sm={9} xs={9}>
+                                                                <Row type="flex">
+                                                                    <h5 className="provider-info-title">DEPARTMENTS</h5>
+                                                                    <Typography className="provider-info">{value.taxonomies[0].desc}</Typography>
+                                                                    <h5 className="provider-info-title">LOCATIONS</h5>
+                                                                    <Typography className="provider-info">{ this.camelCaseAddress(`${value.addresses[0].address_1} ${value.addresses[0].city}`)} </Typography>
+                                                                </Row>
+                                                            </Col>
+                                                        </div>
+                                                    </Row>
+                                                    <Row type="flex">
+                                                        <Col className="view-on-map-col" xxl={18} xl={17} lg={15} md={13} sm={14} xs={15}><h6>View on Map | Directions</h6></Col>
+                                                        <Col xxl={6} xl={7} lg={9} md={11} sm={10} xs={9}>
+                                                            <Button className="button-appointment">
+                                                                <Row type="flex" justify="space-around">
+                                                                    <Col lg={10} md={10} sm={24} xs={24}>Request </Col>
+                                                                    <Col lg={14} md={14} sm={24} xs={24}>appointment</Col>
+                                                                </Row>
+                                                            </Button>
+                                                        </Col>
+                                                    </Row>
                                                 </Col>
                                             </Row>
                                         </Col>
-                                    </Row>
-                                    </Col>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
+                        </Row>
                     </Col>
 
                     <Col lg={12} md={12} sm={12} xs={24}>
@@ -142,10 +148,11 @@ class SearchOnMap extends Component {
                                 google={this.props.google}
                                 center={{ lat: 18.5204, lng: 73.8567 }}
                                 height='530px'
-                                zoom={15} />
+                                zoom={15}
+                            />
                         </div>
                     </Col>
-                 
+
                 </Row>
             </div>
         )
