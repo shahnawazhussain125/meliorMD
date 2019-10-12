b3aeb72303d0b82db097d80eb5bb998f99dc2420
shahnawazhussain125 <shahnawazhussain125@gmail.com>
2019-10-13 02:33:25 +0500

    Updates

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Row, Col, Input, Button, Icon, Tabs, Collapse, Checkbox, Select } from 'antd';
+import { Row, Col, Input, Button, Icon, Tabs, Collapse, Select } from 'antd';
 import State from './searchByState';
+import DentistryFilter from '../Dentistry';
 import { searchByProviderCategoryAndCity } from '../../Redux/Actions/searchActions';
 import { connect } from 'react-redux';
 import 'antd/dist/antd.css';
@@ -59,7 +60,7 @@ class Search extends Component {
     let name = event.target.name;
     let value = event.target.value;
 
-    const { 
+    const {
       general_dentist,
       endodontist,
       oral_and_maxillofacial_surgeon,
@@ -69,7 +70,7 @@ class Search extends Component {
       prosthodontics,
       taxonomy
     } = this.state;
-    let taxonomies = { 
+    let taxonomies = {
       general_dentist,
       endodontist,
       oral_and_maxillofacial_surgeon,
@@ -90,14 +91,14 @@ class Search extends Component {
     else {
       taxonomies[name] = "";
       if (
-        (taxonomies.general_dentist !== "") === 
-        (taxonomies.endodontist !== "") === 
-        (taxonomies.oral_and_maxillofacial_surgeon !== "") === 
-        (taxonomies.orthodontist !== "") === 
-        (taxonomies.pediatric_dentist !== "")  === 
-        (taxonomies.periodontist !== "")  === 
+        (taxonomies.general_dentist !== "") ===
+        (taxonomies.endodontist !== "") ===
+        (taxonomies.oral_and_maxillofacial_surgeon !== "") ===
+        (taxonomies.orthodontist !== "") ===
+        (taxonomies.pediatric_dentist !== "") ===
+        (taxonomies.periodontist !== "") ===
         (taxonomies.prosthodontics !== "")
-        ) {
+      ) {
         this.setState({
           [name]: "", taxonomy: false
         }, () => {
@@ -115,12 +116,12 @@ class Search extends Component {
     }
   }
 
-  handleInputTextChange = name => event =>{
-      this.setState({ 
-        [name]: event.target.value 
-      }, ()=>{
-        this.filterDentist();
-      })
+  handleInputTextChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    }, () => {
+      this.filterDentist();
+    })
   }
 
   handleGenderChange = (event) => {
@@ -184,12 +185,12 @@ class Search extends Component {
         (
           taxonomy ===
           (element.taxonomies[0].desc === general_dentist ||
-          element.taxonomies[0].desc === endodontist ||
-          element.taxonomies[0].desc === oral_and_maxillofacial_surgeon ||
-          element.taxonomies[0].desc === orthodontist ||
-          element.taxonomies[0].desc === pediatric_dentist ||
-          element.taxonomies[0].desc === periodontist ||
-          element.taxonomies[0].desc === prosthodontics)
+            element.taxonomies[0].desc === endodontist ||
+            element.taxonomies[0].desc === oral_and_maxillofacial_surgeon ||
+            element.taxonomies[0].desc === orthodontist ||
+            element.taxonomies[0].desc === pediatric_dentist ||
+            element.taxonomies[0].desc === periodontist ||
+            element.taxonomies[0].desc === prosthodontics)
         )
         &&
         element.basic.name.toLowerCase().indexOf(practiceName.toLowerCase()) !== -1
@@ -199,7 +200,7 @@ class Search extends Component {
         element.addresses[0].city.toLowerCase().indexOf(stateName.toLowerCase()) !== -1
         &&
         element.addresses[0].postal_code.toLowerCase().indexOf(zipCode.toLowerCase()) !== -1
-        && 
+        &&
         (gender === (element.basic.gender === male || element.basic.gender === female))
       )
     })
@@ -219,7 +220,6 @@ class Search extends Component {
                 <h1 className="text-style1">Find your best Physicians</h1>
                 <h2 className="text-style2">meliorMD will help you in that important decision</h2>
                 <Row type="flex" justify="center" style={{ padding: '10px' }}>
-
                   <Col md={10} sm={10} xs={23}>
                     <Row type="flex" justify="center">
                       <div className="div-input1">
@@ -264,164 +264,40 @@ class Search extends Component {
                     </Row>
                   </Col>
                 </Row>
-                {
-                  <Row type="flex" justify="center">
-                    <Col span={24}>
-                      <Collapse className="collapse-tab" defaultActiveKey={['1']}>
-                        <Panel header="Advanced Search" key="1">
-                          <div style={{ backgroundColor: '#ffffff', height: 'auto' }}>
-                            <Row>
-                              <h3 style={{ color: '#0D47A1' }}>Find a listing</h3>
-                            </Row>
-
-                            <div className="card-container">
-                              <Tabs type="card">
-                                <TabPane tab="Dentistry" key="1">
-                                  <Row type="flex">
-                                    <Col span={12} xs={23}>
-                                      <div className="check-box-container">
-                                        <Checkbox className="check-box" onChange={this.handleTaxonomyChange} name="general_dentist" value="Dentist General Practice"  >
-                                          <span className="check-box-text">General Dentist</span>
-                                        </Checkbox><br />
-                                        <Checkbox className="check-box" onChange={this.handleTaxonomyChange} name="endodontist" value="Endodontist">
-                                          <span className="check-box-text">Endodontist</span>
-                                        </Checkbox><br />
-                                        <Checkbox className="check-box" onChange={this.handleTaxonomyChange} name="oral_and_maxillofacial_surgeon" value="Dentist Oral and Maxillofacial Surgery">
-                                          <span className="check-box-text">Oral and Maxillofacial Surgeon</span>
-                                        </Checkbox><br />
-                                        <Checkbox className="check-box" onChange={this.handleTaxonomyChange} name="orthodontist" value="Orthodontist">
-                                          <span className="check-box-text">Orthodontist</span>
-                                        </Checkbox><br />
-                                      </div>
-                                    </Col>
-
-                                    <Col span={12} xs={23}>
-                                      <div className="check-box-container2">
-                                        <Checkbox className="check-box" onChange={this.handleTaxonomyChange} name="pediatric_dentist" value="Pediatric Dentist">
-                                          <span className="check-box-text">Pediatric Dentist</span>
-                                        </Checkbox><br />
-                                        <Checkbox className="check-box" onChange={this.handleTaxonomyChange} name="periodontist" value="Periodontist">
-                                          <span className="check-box-text">Periodontist</span>
-                                        </Checkbox><br />
-                                        <Checkbox className="check-box" onChange={this.handleTaxonomyChange} name="prosthodontics" value="Dentist Prosthodontics">
-                                          <span className="check-box-text">Prosthodontics </span>
-                                        </Checkbox>
-                                      </div>
-                                    </Col>
-                                  </Row>
-
-                                  <Row type="flex" justify="center">
-                                    <Col span={24}>
-                                      <div style={{ padding: '10px' }}>
-                                        <label><h5 style={{ color: '#AEB1B1' }}>HOSPITAL/PRACTICE NAME</h5></label>
-                                        <Input
-                                          placeholder="Search by business name"
-                                          onChange={this.handleInputTextChange("practiceName")}
-                                        />
-                                        <br />
-                                        <br />
-
-                                        <Col span={11}>
-                                          <div>
-                                            <label><h5 style={{ color: '#AEB1B1' }}>CITY OR STATE</h5></label>
-                                            <Input placeholder="e.g California" onChange={this.handleInputTextChange("stateName")} />
-                                          </div>
-                                        </Col>
-
-                                        <Col span={2}>
-                                        </Col>
-
-                                        <Col span={11}>
-                                          <div>
-                                            <label><h5 style={{ color: '#AEB1B1' }}>ZIP CODE</h5></label>
-                                            <Input placeholder="e.g California" onChange={this.handleInputTextChange("zipCode")} />
-                                          </div>
-                                        </Col>
-                                      </div>
-                                    </Col>
-                                  </Row><br />
-
-                                  <Row type="flex" justify="center">
-                                    <Col span={24}>
-                                      <h6 style={{ color: '#AEB1B1' }}>TYPE OF PATIENT SERVED</h6>
-                                      <div style={{ padding: '10px' }}>
-                                        <Checkbox style={{ marginBottom: '10px' }}>
-                                          <span className="check-box-text">Adults Only </span>
-                                        </Checkbox><br />
-                                        <Checkbox style={{ marginBottom: '10px' }}>
-                                          <span className="check-box-text">Both Adults and Children &  </span>
-                                        </Checkbox><br />
-                                        <Checkbox style={{ marginBottom: '10px' }}>
-                                          <span className="check-box-text">Children & Adolescents Only </span>
-                                        </Checkbox>
-                                      </div>
-                                    </Col>
-                                  </Row>
-
-                                  <Row type="flex" justify="center">
-                                    <Col span={24}>
-                                      <h6 style={{ color: '#AEB1B1' }}>GENDER OF HEALTHCARE PROVIDER</h6>
-                                      <div style={{ padding: '10px' }}>
-                                        <Checkbox
-                                          style={{ marginBottom: '10px' }}
-                                          name="female"
-                                          value="F"
-                                          onChange={this.handleGenderChange}
-                                        >
-                                          <span className="check-box-text">Female </span>
-                                        </Checkbox><br />
-                                        <Checkbox
-                                          style={{ marginBottom: '10px' }}
-                                          name="male"
-                                          value="M"
-                                          onChange={this.handleGenderChange}
-                                        >
-                                          <span className="check-box-text">Male </span>
-                                        </Checkbox>
-                                      </div>
-                                    </Col>
-                                  </Row>
-
-                                  <Row type="flex" justify="center">
-                                    <Col span={24}>
-                                      <div>
-                                        <Button className="button-style2">
-                                          SHOW HEALTHCARE PROVIDERS ({this.state.filterData.length} RESULTS)
-                                      </Button>
-                                      </div>
-                                    </Col>
-                                  </Row>
-
-                                  <Col span={24}>
-                                    <State history={this.props.history} />
-                                  </Col>
-                                </TabPane>
-
-                                <TabPane tab="Physicians" key="2">
-                                  <p>Content of Tab Pane 2</p>
-                                  <p>Content of Tab Pane 2</p>
-                                  <p>Content of Tab Pane 2</p>
-                                </TabPane>
-
-                                <TabPane tab="Other Healthcare Providers" key="3">
-                                  <p>Content of Tab Pane 3</p>
-                                  <p>Content of Tab Pane 3</p>
-                                  <p>Content of Tab Pane 3</p>
-                                </TabPane>
-                              </Tabs>
-                            </div>
-                          </div>
-                        </Panel>
-                      </Collapse>
-                    </Col>
-
-                    <Col span={1}></Col>
-                  </Row>
-                }
               </span>
             </div>
           </Col>
         </Row>
+        <Row className="row-container">
+          <Col lg={12} md={15} sm={20} xs={23}>
+            <Row className="find-a-listing">
+                <h3>Find a listing</h3>             
+            </Row>
+            <Row className="card-container filter-container" >
+              <Tabs type="card">
+                <TabPane tab="Dentistry" key="1">
+                  <DentistryFilter />
+                </TabPane>
+
+                <TabPane tab="Physicians" key="2">
+                  <p>Content of Tab Pane 2</p>
+                  <p>Content of Tab Pane 2</p>
+                  <p>Content of Tab Pane 2</p>
+                </TabPane>
+
+                <TabPane tab="Other Healthcare Providers" key="3">
+                  <p>Content of Tab Pane 3</p>
+                  <p>Content of Tab Pane 3</p>
+                  <p>Content of Tab Pane 3</p>
+                </TabPane>
+              </Tabs>
+            </Row>
+            <Row>
+              <State history={this.props.history} />
+            </Row>
+          </Col>
+        </Row>
+
       </div>
     )
   }
