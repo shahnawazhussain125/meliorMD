51f54db59bd49c633a88791b17b3aca2730ffbc1
shahnawazhussain125 <shahnawazhussain125@gmail.com>
2019-12-29 12:51:13 +0500

    Today Commit

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import searchReducer from './searchReducers';
 import authReducer from './authReducers'
+import bookingReducer from './bookingReducer';
 
 const rootReducer = combineReducers({
     searchReducer,
-    authReducer
+    authReducer,
+    bookingReducer
 })
 
 export default rootReducer;
